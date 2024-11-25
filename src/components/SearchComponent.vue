b70951889c7c43152dbdb5e6d59514871700734d
mgilyazov <mgilyazov@phoenixit.ru>
2024-11-25 18:34:14 +0300

    Домашка №3 Подправил поиск

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,8 +1,9 @@
 <template>
-  <div style="color: black; padding-top: 7px;">
+  <div style="color: black; ">
 
     <input
         type="text"
+        class="mt-1 block w-full border-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 h-11 px-3"
         v-model="localSearchTerm"
         placeholder="Поиск по названию или описанию..."
         @input="onInput"
--- src/components/ShoppingCart.vue
+++ src/components/ShoppingCart.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative inline-block">
+  <div class="relative inline-block py-1">
     <button class="flex items-center px-4 py-2 text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">
       <span>Корзина </span>
       <span class="ml-2 inline-flex items-center justify-center w-5 h-5 text-xs font-semibold text-blue-600 bg-white rounded-full border border-blue-600">{{props.tovar.length}}</span>
